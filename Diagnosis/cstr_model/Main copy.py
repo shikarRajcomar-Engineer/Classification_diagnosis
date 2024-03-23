1e9d33576b77aa32cf5b89c48674b6ed3775b6ac
shikar rajcomar <shikar.rajcomar@gmail.com>
2024-03-23 21:19:50 +0200

    add Mdistance

--- Diagnosis/cstr_model/Main copy.py
+++ Diagnosis/cstr_model/Main copy.py
@@ -34,13 +34,12 @@ from sklearn.metrics import (confusion_matrix, precision_recall_curve, auc,
                              precision_recall_fscore_support)
 from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
 from sklearn.metrics import mean_squared_error
-
+import Utils
 # Hide Warnings
 import os
+import warnings
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'   #To enable them in non-MKL-DNN operations, rebuild TensorFlow with the appropriate compiler flags.
-
-
-
+warnings.filterwarnings('ignore', category=UserWarning, module='openpyxl')
 
 
 
@@ -49,71 +48,17 @@ x = df[df.columns[2:9]].to_numpy()
 scaler = preprocessing.StandardScaler()
 scaled_data = scaler.fit_transform(x)
 train_data, test_data = train_test_split(scaled_data, test_size=0.3)
-n_features = train_data.shape[1]  
+n_features = train_data.shape[1]
 
 
-for i in range(n_features):
-    encoder = keras.Sequential(name='encoder')
-    encoder.add(keras.layers.Dense(units=64, activation=keras.activations.relu, input_shape=[n_features]))
-    encoder.add(keras.layers.BatchNormalization())
-    encoder.add(keras.layers.Dropout(0.2))
-    encoder.add(keras.layers.Dense(units=32, activation=keras.activations.relu))
-    encoder.add(keras.layers.BatchNormalization())
-    encoder.add(keras.layers.Dropout(0.2))
-    encoder.add(keras.layers.Dense(units=16, activation=keras.activations.relu))
-    encoder.add(keras.layers.BatchNormalization())
-    encoder.add(keras.layers.Dropout(0.2))
-
-    decoder = keras.Sequential(name='decoder')
-    decoder.add(keras.layers.Dense(units=32, activation=keras.activations.relu, input_shape=[16]))
-    decoder.add(keras.layers.BatchNormalization())
-    decoder.add(keras.layers.Dropout(0.2))
-    decoder.add(keras.layers.Dense(units=64, activation=keras.activations.relu))
-    decoder.add(keras.layers.BatchNormalization())
-    decoder.add(keras.layers.Dropout(0.2))
-    decoder.add(keras.layers.Dense(units=n_features, activation=keras.activations.sigmoid))
-    decoder.add(keras.layers.BatchNormalization())
-
-    autoencoder = keras.Sequential([encoder, decoder])
-
-    loss = keras.losses.Huber()
-    learning_rate = 0.001
-    optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
-    es = EarlyStopping(monitor='val_loss', min_delta=0.00001, patience=20, restore_best_weights=True)
-
-    autoencoder.compile(
-        loss=loss,
-        optimizer=optimizer,
-        metrics=[keras.metrics.MeanSquaredError()])
+Utils.Model_development(n_features,train_data, test_data)
 
 
-    history = autoencoder.fit(
-        x=train_data, y=train_data,
-        batch_size=32,
-        epochs=50,
-        verbose=0,
-        validation_data=(test_data, test_data),
-        callbacks=[es])
-
-    # Plot the learning curves
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('Model Loss')
-    plt.ylabel('Loss')
-    plt.xlabel('Epoch')
-    plt.legend(['Train', 'Test'], loc='upper left')
-    plt.show()
-
-
-best_model = keras.models.clone_model(autoencoder)
-best_model.set_weights(autoencoder.get_weights())
-best_model.save(f'AE_feature{i}.h5')
-
-# autoencoder=load_model('best_model2.h5')
+autoencoder=load_model('best_model2.h5')
 
 
 
-# Im importing a test file and scaling it then passing the scaled values into the model the model predictions is then inversed to reconstruct the original data(y pred)
+# I am importing a test file and scaling it then passing the scaled values into the model the model predictions is then inversed to reconstruct the original data(y pred)
 # Overall mse is calculated and each signal mse is calculated(MSE vs MAE?)
 # All datapointws are being passed but can also only pass Class =1 Anomalies
 
@@ -132,56 +77,3 @@ Overall_MSE=pd.DataFrame(Overall_MSE,columns=['Overall Error'])
 
 
 ypred=pd.DataFrame(Predicted_original)
-ypred.to_excel('ypred.xlsx')
-
-# y=pd.DataFrame(original)
-# mse_df = pd.DataFrame(index=y.index)
-
-# # Calculate MSE for each column
-# for column in y.columns:
-#     mse_df[column] = [(y_i - ypred_i) ** 2 for y_i, ypred_i in zip(y[column], ypred[column])]
-# Results_df=pd.concat([mse_df,Overall_MSE],axis=1)
-# # dfx=df[['Ci','Ti','Tci','Tsp','Qc','Tc','T']]
-
-# # Extract sensor error and plot distribution
-
-# sensor_errors=Results_df.drop(Overall_MSE,axis=1)
-# Overall_MSE=Results_df['Overall Error']
-
-# for col in sensor_errors.columns:
-#     plt.hist(sensor_errors[col],label=col,alpha=0.7)
-# # plt.hist(Overall_MSE,label='Overall Error',alpha=0.7)
-# plt.xlabel('Reconstruction Error(MSE)')
-# plt.ylabel('Frequency')
-# plt.title('Distribution of Reconstruction errror')
-# plt.legend()
-# # plt.show()
-
-
-# fig,axis=plt.subplots(nrows=len(sensor_errors.columns),figsize=(10,8))
-# for i, (col, ax) in enumerate(zip(sensor_errors.columns, axis[:-1])):
-#     ax.hist(sensor_errors[col],label=col,alpha=0.7)
-#     ax.set_xlabel('Reconsturction error(MSE)')
-#     ax.set_ylabel('Frequency')
-#     ax.set_title(f'Reconstruction error for {col}')
-
-# fig.tight_layout()
-# # plt.show()
-
-
-# # Define a function to calculate statistics
-# def calculate_stats(series):
-#     range = series.max() - series.min()
-#     iqr = series.quantile(0.75) - series.quantile(0.25)
-#     sd = series.std()
-#     cv = series.std() / series.mean()
-#     return range, iqr, sd, cv
-
-# # Calculate and print statistics for each sensor
-# for col in sensor_errors.columns:
-#     range, iqr, sd, cv = calculate_stats(sensor_errors[col])
-#     print(f"\nSensor {col}:")
-#     print(f"  Range: {range:.4f}")
-#     print(f"  IQR: {iqr:.4f}")
-#     print(f"  SD: {sd:.4f}")
-#     print(f"  CV: {cv:.4f}")
--- Diagnosis/cstr_model/Utils.py
+++ Diagnosis/cstr_model/Utils.py
@@ -1,29 +1,46 @@
-# Import necessary libraries
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import (
-accuracy_score,
-precision_score,
-recall_score,
-f1_score,
-confusion_matrix,
-plot_confusion_matrix,
-plot_roc_curve,
-precision_recall_curve,
-average_precision_score)
-import os
+
+import numpy as np
+from numpy import ma
 import pandas as pd
-import seaborn as sns
+import math
+import matplotlib
 import matplotlib.pyplot as plt
-from sklearn.svm import SVC
-from sklearn.preprocessing import label_binarize
-from itertools import cycle
-import tensorflow as tf 
+import matplotlib.colors as colors
+from matplotlib import ticker, cm
+from matplotlib.pyplot import figure
+import seaborn as sns
+import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.layers import Dense, Dropout, Input
+from tensorflow.keras import regularizers
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+from sklearn.model_selection import train_test_split
+import sklearn
+from sklearn import datasets
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn import decomposition
+import scipy
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn import datasets, decomposition, preprocessing
+from sklearn.model_selection import KFold
+
+# from model import Autoencoder
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import (confusion_matrix, precision_recall_curve, auc,
+                             roc_curve, recall_score, classification_report, f1_score,
+                             precision_recall_fscore_support)
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import mean_squared_error
+
+# Hide Warnings
+import os
+import warnings
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'   #To enable them in non-MKL-DNN operations, rebuild TensorFlow with the appropriate compiler flags.
+warnings.filterwarnings('ignore', category=UserWarning, module='openpyxl')
+
 
 
 # Prepare the dataset
@@ -41,6 +58,58 @@ from tensorflow.keras.layers import Dense
 #             masterfile=masterfile.append(df)
 #     masterfile=masterfile.iloc[:,2:]
 #     return masterfile
+
+def Model_development(n_features,train_data, test_data):
+    encoder = keras.Sequential(name='encoder')
+    encoder.add(layer=keras.layers.Dense(units=20, activation=keras.activations.relu, input_shape=[n_features]))
+    encoder.add(keras.layers.Dropout(0.1))
+    encoder.add(layer=keras.layers.Dense(units=10, activation=keras.activations.relu))
+    encoder.add(layer=keras.layers.Dense(units=5, activation=keras.activations.relu))
+
+    decoder = keras.Sequential(name='decoder')
+    decoder.add(layer=keras.layers.Dense(units=10, activation=keras.activations.relu, input_shape=[5]))
+    decoder.add(layer=keras.layers.Dense(units=20, activation=keras.activations.relu))
+    decoder.add(keras.layers.Dropout(0.1))
+    decoder.add(layer=keras.layers.Dense(units=n_features, activation=keras.activations.sigmoid))
+
+    autoencoder = keras.Sequential([encoder, decoder])
+
+    autoencoder.compile(
+        loss=keras.losses.MSE,
+        optimizer=keras.optimizers.Adam(),
+        metrics=[keras.metrics.mean_squared_error,'accuracy'])
+
+    loss = keras.losses.Huber()
+    learning_rate = 0.001
+    optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
+    es = EarlyStopping(monitor='val_loss', min_delta=0.00001, patience=20, restore_best_weights=True)
+
+    history = autoencoder.fit(
+        x=train_data, y=train_data,
+        batch_size=32,
+        epochs=50,
+        verbose=0,
+        validation_data=(test_data, test_data),
+        callbacks=[es])
+
+    # # Plot the learning curves
+    # plt.plot(history.history['loss'])
+    # plt.plot(history.history['val_loss'])
+    # plt.title('Model Loss')
+    # plt.ylabel('Loss')
+    # plt.xlabel('Epoch')
+    # plt.legend(['Train', 'Test'], loc='upper left')
+    # plt.show()
+
+# Save the best model
+    best_model = keras.models.clone_model(autoencoder)
+    best_model.set_weights(autoencoder.get_weights())
+    best_model.save('AE_feature.h5')
+
+    return history
+
+
+
 
 def plot_training_history(history):
     # Plot training history
